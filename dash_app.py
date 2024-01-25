a0e1ecafaffe982e12e5a47ba8982cf60f45aa06
ugenpact_164471811090 <ugenpact_164471811090@npunext.onmicrosoft.com>
2024-01-25 13:07:35 +0530

    added day2

--- dash_app.py
+++ dash_app.py
@@ -7,7 +7,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    html.H1(children='Country Graph', style={'textAlign': 'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
@@ -18,7 +18,7 @@ app.layout = html.Div([
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
+    dff = df[df.country == value]
     return px.line(dff, x='year', y='pop')
 
 
